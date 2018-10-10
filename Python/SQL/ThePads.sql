7d639c34316cc48eb74b874c2cbce523a3a26b82
Dhanshri Kekre <40416930+dkekre21@users.noreply.github.com>
2018-10-09 20:40:47 -0700

    Delete ThePads.sql

--- Python/SQL/ThePads.sql
+++
@@ -1,5 +0,0 @@
-select Concat(name,"(",substr(occupation,1,1),")") from occupations
-order by name;
-select Concat("There are a total of ",count(lower(occupation))," ", lower(occupation),"s.") from occupations
-group by occupation
-order by count(occupation) asc, occupation;
